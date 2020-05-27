fdfe91d88b6488809cf51f4b1de3cc5813f35b3b
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-05-27 21:05:14 +0200

    prepare server

---
+++ server/serve.sh
@@ -0,0 +1,6 @@
+systemctl start docker.service &&
+docker pull tensorflow/serving &&
+docker run -it --rm -p 8501:8501 \
+-v "$(realpath $(dirname $0))/model:/models/hate" \
+-e MODEL_NAME=hate \
+tensorflow/serving
